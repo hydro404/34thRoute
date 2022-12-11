b63e021afe9a77aef29645cbaa3c7d868adf8fc2
benny <benedickbayaban.binlayo@bicol-u.edu.ph>
2022-12-12 01:38:32 +0800

    gfghfghfghfg

--- dist/js/create-source.js
+++ dist/js/create-source.js
@@ -115,8 +115,8 @@ async function createSource(
         attributes: {
           amount: amount,
           redirect: {
-            success: "https://paymongo-kasmagtech.web.app/account-orders.html",
-            failed: "https://paymongo-kasmagtech.web.app/account-orders.html",
+            success: "https://34throute.online/account-orders.html",
+            failed: "https://34throute.online/account-orders.html",
           },
           billing: {
             address: {
